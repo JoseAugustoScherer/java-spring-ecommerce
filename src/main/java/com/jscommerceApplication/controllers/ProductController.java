95fd4599f4335f5c4b45120d79358d1cb0d18789
Jose Augusto Scherer <jose.augusto.github@gmail.com>
2025-10-15 13:32:27 -0300

    Added custom responses with ResponseEntity

--- src/main/java/com/jscommerceApplication/controllers/ProductController.java
+++ src/main/java/com/jscommerceApplication/controllers/ProductController.java
@@ -5,8 +5,11 @@ import com.jscommerceApplication.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -27,18 +30,22 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping ( value = "/{id}")
-    public ProductDTO findById( @PathVariable Long id ) {
-        return service.findById( id );
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id ) {
+        ProductDTO dto = service.findById( id );
+        return ResponseEntity.ok( dto );
     }
 
     @GetMapping
-    public Page<ProductDTO> findAll( Pageable pageable ) {
-        return service.findAll( pageable );
+    public ResponseEntity<Page<ProductDTO>> findAll( Pageable pageable ) {
+        Page<ProductDTO> dto = service.findAll( pageable );
+        return ResponseEntity.ok( dto );
     }
 
     @PostMapping
-    public ProductDTO createProduct( @RequestBody ProductDTO productDTO ) {
-        return service.createProduct( productDTO );
+    public ResponseEntity<ProductDTO> createProduct( @RequestBody ProductDTO productDTO ) {
+        ProductDTO dto = service.createProduct( productDTO );
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path( "/{id}").buildAndExpand( dto.getId() ).toUri();
+        return ResponseEntity.created( uri ).body( dto );
     }
 
 }
