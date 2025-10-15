129177dd90065dfcfc66eb0e5cc7b1f8884167fa
Jose Augusto Scherer <jose.augusto.github@gmail.com>
2025-10-15 09:47:50 -0300

    Implemented search for all records with pagination

--- src/main/java/com/jscommerceApplication/services/ProductService.java
+++ src/main/java/com/jscommerceApplication/services/ProductService.java
@@ -4,9 +4,12 @@ import com.jscommerceApplication.dto.ProductDTO;
 import com.jscommerceApplication.entities.Product;
 import com.jscommerceApplication.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -26,10 +29,14 @@ public class ProductService {
 
     @Transactional ( readOnly = true )
     public ProductDTO findById( Long id ) {
-        Optional<Product> result = productRepository.findById( id );
-        Product product = result.get();
-        ProductDTO productDTO = new ProductDTO( product );
-        return productDTO;
+        Product product = productRepository.findById( id ).get();
+        return new ProductDTO( product );
+    }
+
+    @Transactional ( readOnly = true )
+    public Page<ProductDTO> findAll( Pageable pageable ) {
+        Page<Product> products = productRepository.findAll( pageable );
+        return products.map( x -> new ProductDTO( x ) );
     }
 
 }
